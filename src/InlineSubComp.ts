7357d63189a436597675cf08d33893ae72c11ae7
huiquan <huiquan@implustech.com>
2016-09-01 17:25:47 +0800

    fix: dir move

---
+++ src/InlineSubComp.ts
@@ -0,0 +1,7 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'inline-sub-comp',
+  template: `<div>This component was loaded inline.</div>`
+})
+export default class InlineSubComp {}
